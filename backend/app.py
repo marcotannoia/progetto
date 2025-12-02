34780ec99d42022ab16bf7eac56cdae59ad75f41
Marco <m.tannoia@studenti.poliba.it>
2025-12-02 09:03:29 +0100

    introduzione api google maps

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,14 @@
 from flask import Flask, jsonify
 from flask_cors import CORS # Importante per far parlare React e Python
+import os
+
+# Legge la variabile dal sistema operativo del container
+API_KEY = os.environ.get("GOOGLE_API_KEY") 
+
+if not API_KEY:
+    raise ValueError("Attenzione: Manca la API Key di Google!")
+
+# -- fine gestione API Key --
 
 app = Flask(__name__)
 CORS(app) # Abilita le chiamate dal frontend
@@ -11,5 +20,5 @@ def saluta():
     dati = {"messaggio": "Ciao Marco! Questi dati arrivano da Python!"}
     return jsonify(dati)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__': #
+    app.run(host='0.0.0.0', port=50000) #ogni function backend ha la sua porta
